f5120f1178dacb32354d30f96ba1dc56dd7bfa59
Isal0192 <117461239+Isal0192@users.noreply.github.com>
2024-05-16 15:08:08 +0700

    Update character.py
    key left and right only

--- character.py
+++ character.py
@@ -44,32 +44,26 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-    # Handling key inputs
+    
+    # Handling key 
+    
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= vel
-        framet = (frame + 1) % len(coich[0])
-        toWindow = window.blit(walkr[frame], (x, y))
     if keys[pygame.K_RIGHT]:
         x += vel
         frame = (frame + 1) % len(coich[1])
         toWindow = window.blit(walkr[frame], (x, y))
-    if keys[pygame.K_UP]:
-        y -= vel
-        frame = (frame + 1) % len(walkr)
-    if keys[pygame.K_DOWN]:
-        y += vel
-        frame = (frame + 1) % len(walkr)
-
+        pygame.display.update()
+    if keys[pygame.K_LEFT]:
+        x -= vel
+        frame = (frame - 1) % len(walkl)
+        toWindow = window.blit(walkl[frame], (x, y))
+        pygame.display.update()
+        
     # Update frame index for animation
 
     # Clear the screen
     window.fill((0, 0, 0))
 
-    # Draw the current frame of the walk cycle at the character's position
-    toWindow = window.blit(walkr[frame], (x, y))
-    # Update the display
-    pygame.display.update()
 
+    
 pygame.quit()
